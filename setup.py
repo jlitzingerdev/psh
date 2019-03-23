475a86512927f25b0e93ed364cb1291558712d54
Jason Litzinger <jlitzingerdev@gmail.com>
2019-03-23 15:22:09 -0600

    fix: Only python 3.6 is known to work.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
     ],
     keywords="dependency setuptools development",
     packages=find_packages(where="src"),
